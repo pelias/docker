9b6e383ceb6f35bf664e238b3ef5f191271c76d8
John Wileczek <john.wileczek@prominentedge.com>
2018-10-02 13:19:13 -0400

    Add acceptance-tests docker service, and pelias cli command to run acceptance-tests test suite.

--- cmd/test.sh
+++ cmd/test.sh
@@ -3,5 +3,7 @@ set -e;
 
 # run acceptance tests
 function test_fuzzy(){ compose_run 'fuzzy-tester' -e 'docker' $@; }
+function test_acceptance() { compose_run 'acceptance-tests' npm test -- -e 'docker' $@; }
 
 register 'test' 'run' 'run fuzzy-tester test cases' test_fuzzy
+register 'test' 'acceptance-tests' 'run acceptance-tests test cases' test_acceptance
