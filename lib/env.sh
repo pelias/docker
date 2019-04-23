c7913ae8e604604860d7086744ff56912d65758e
Pascal Christoph <christoph@hbz-nrw.de>
2019-04-23 14:33:30 +0200

    Move warning when DATA_DIR is set twice
    The warning will only show up when it's necessary.
    
    See #93.

--- lib/env.sh
+++ lib/env.sh
@@ -24,6 +24,7 @@ function env_check(){
 # loads environment vars from a stream (such as a file)
 # example: env_load_stream < .env
 function env_load_stream(){
+  [[ -n $DATA_DIR ]] && printf "DATA_DIR is already set to '$DATA_DIR' - this may cause the DATA_DIR specified in the .env to be ignored\n"
   while IFS='=' read -r key value; do
     ([ -z $key ] || [ -z $value ]) && printf 'Invalid environment var "%s=%s"\n' $key $value && exit 1
     if [ -z ${!key} ]; then
@@ -37,8 +38,6 @@ function env_load_stream(){
 # ensure locale is correctly set?
 # export LC_ALL=en_US.UTF-8
 
-# warn if DATA_DIR is already set
-[[ -n $DATA_DIR ]] && printf "DATA_DIR is already set to '$DATA_DIR' - this may cause the DATA_DIR specified in the .env to be ignored\n"
 # load DATA_DIR and other vars from docker-compose .env file
 # note: strips comments and empty lines
 [ -f .env ] && env_load_stream < <(grep -v '^$\|^\s*$\#' .env)
