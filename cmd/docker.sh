3f5d8095fa52d36223a68d5289cda732f2bb3f53
Wel Rachid <wel@rachid.dk>
2022-11-06 11:40:45 +0100

    Docker compose part of docker
    docker-compose is no longer standalone. It comes with docker, and so should the commands be.

--- cmd/docker.sh
+++ cmd/docker.sh
@@ -17,18 +17,18 @@ register 'compose' 'ps' 'list containers' compose_ps
 function compose_top(){ compose_exec top $@; }
 register 'compose' 'top' 'display the running processes of a container' compose_top
 
-function compose_exec(){ docker-compose $@; }
-register 'compose' 'exec' 'execute an arbitrary docker-compose command' compose_exec
+function compose_exec(){ docker compose $@; }
+register 'compose' 'exec' 'execute an arbitrary docker compose command' compose_exec
 
-function compose_run(){ net_init; docker-compose run --rm $@; }
-register 'compose' 'run' 'execute a docker-compose run command' compose_run
+function compose_run(){ net_init; docker compose run --rm $@; }
+register 'compose' 'run' 'execute a docker compose run command' compose_run
 
-function compose_up(){ docker-compose up -d $@; }
-register 'compose' 'up' 'start one or more docker-compose service(s)' compose_up
+function compose_up(){ docker compose up -d $@; }
+register 'compose' 'up' 'start one or more docker compose service(s)' compose_up
 
-function compose_kill(){ docker-compose kill $@; }
-register 'compose' 'kill' 'kill one or more docker-compose service(s)' compose_kill
+function compose_kill(){ docker compose kill $@; }
+register 'compose' 'kill' 'kill one or more docker compose service(s)' compose_kill
 
-function compose_down(){ docker-compose down; }
-register 'compose' 'down' 'stop all docker-compose service(s)' compose_down
+function compose_down(){ docker compose down; }
+register 'compose' 'down' 'stop all docker compose service(s)' compose_down
 
